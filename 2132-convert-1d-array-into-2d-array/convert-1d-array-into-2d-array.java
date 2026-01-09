8b548d8fe550d7f9991239b70e8994867b65827a
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-01-09 23:33:48 +0530

    Time: 2 ms (100.00%) | Memory: 67.2 MB (42.22%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int x=original.length;
+        if(m*n != x){
+            return new int[0][0];
+        }
+        int[][] ans = new int[m][];
+        for(int i=0;i<m;i++){
+            ans[i] = Arrays.copyOfRange(original,i*n , i*n+n);
+        }
+        return ans;
+    }
+}
