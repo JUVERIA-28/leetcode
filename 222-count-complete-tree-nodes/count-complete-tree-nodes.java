87f3dac9bc44a53d196f6d76c7e60430b3a8ee62
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-01-31 15:05:52 +0530

    Time: 0 ms (100.00%) | Memory: 49.3 MB (79.87%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int l=0;
+        TreeNode t=root;
+        while(t!=null){
+            l++;
+            t=t.left;
+        }
+        TreeNode t1=root;
+        int r=0;
+        while(t1!=null){
+            r++;
+            t1=t1.right;
+        }
+        if(l==r){
+            return (1<<l)-1;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
