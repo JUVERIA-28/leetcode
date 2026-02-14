4f7a4d28a90984b7d359321a7e266922bd3add0d
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-02-14 17:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (97.44%) - LeetSync

---
+++ 476-number-complement/number-complement.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findComplement(int num) {
+        if(num==0){
+            return 1;
+        }
+        int len = Integer.toBinaryString(num).length();
+        int com=(1<<len)-1;
+
+        return num^com;
+    }
+}
