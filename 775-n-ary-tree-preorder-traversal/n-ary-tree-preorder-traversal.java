ccd2d0238412f01a9867685c0532c7eefa5a52b3
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-02-27 10:31:56 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (71.65%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.java
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        help(root,ans);
+        return ans;
+    }
+    void help(Node root,List<Integer> ans){
+        if(root==null){
+            return;
+        }
+        ans.add(root.val);
+        for(Node c : root.children){
+            help(c,ans);
+        }
+    }
+}
