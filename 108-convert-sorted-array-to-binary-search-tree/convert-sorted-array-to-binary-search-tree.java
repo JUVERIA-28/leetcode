4fd0699864d5bb77992d04bf1d300627acc40e57
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-03-15 20:49:42 +0530

    Time: 1 ms (2.26%) | Memory: 44.8 MB (88.93%) - LeetSync

---
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return help(nums,0,nums.length-1);
+    }
+    private TreeNode help(int[] nums,int l,int r){
+        if(l>r){
+            return null;
+        }
+        int m=(l+r)/2;
+        TreeNode rt=new TreeNode(nums[m]);
+        rt.left=help(nums,l,m-1);
+        rt.right=help(nums,m+1,r);
+        return rt;
+    }
+}
