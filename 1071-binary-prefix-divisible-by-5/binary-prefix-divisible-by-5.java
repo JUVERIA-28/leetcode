c16cd83909e4bbbb3462e4b3f33284b78b0edb67
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-04-09 21:16:12 +0530

    Time: 3 ms (99.45%) | Memory: 47.9 MB (17.71%) - LeetSync

---
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> res = new ArrayList<>();
+        int v=0;
+        for(int n:nums){
+            v=((v*2)+n)%5;
+            res.add(v==0);
+        }
+        return res;
+    }
+}
