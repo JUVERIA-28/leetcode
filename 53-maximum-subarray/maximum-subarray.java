1ebab40e48974010855984a8bfebcc9d651ea237
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-04-28 10:39:07 +0530

    Time: 1 ms (99.97%) | Memory: 76.9 MB (91.80%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cs=nums[0];
+        int mxs=nums[0];
+        for(int i=1;i<nums.length;i++){
+            cs=Math.max(nums[i],cs+nums[i]);
+            mxs=Math.max(mxs,cs);
+        }
+        return mxs;
+        
+    }
+}
