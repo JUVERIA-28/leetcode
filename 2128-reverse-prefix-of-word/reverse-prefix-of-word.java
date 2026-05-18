ed1fcd407ba326a0b418db028b55f65a584d3ed2
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-05-18 18:38:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (26.24%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int idx=word.indexOf(ch);
+        if(idx!=-1){
+            return new StringBuilder(word.substring(0,idx+1)).reverse().toString()+word.substring(idx+1);
+        }
+        return word;
+    }
+}
