cab90d4ea0b8ca0d0f5b528fe4abae839cfc024a
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-07-15 20:44:56 +0530

    Time: 1 ms (83.11%) | Memory: 42.8 MB (20.94%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) 
+    {
+        /*int odd=0,even=0;
+        int i=1;
+        while(odd<4 && even<4)
+        {
+            if(i%2!=0)
+            {
+                odd+=i;
+            }else{
+                even+=i;
+            }
+            i++;
+        }*/
+        return n;
+        
+    }
+    
+}
