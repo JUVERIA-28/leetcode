fb2ea38344e32435d17d039feb3d2f40a3b122e2
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-07-18 19:59:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (29.34%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePaths(int m, int n)
+    {
+        int[][] mt=new int[m][n];
+        for(int i=0;i<m;i++)
+        {
+            mt[i][0]=1;
+        }
+        for(int i=0;i<n;i++)
+        {
+            mt[0][i]=1;
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++){
+                mt[i][j]=mt[i-1][j]+mt[i][j-1];
+            }
+        }
+        return mt[m-1][n-1];
+        
+    }
+}
