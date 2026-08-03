576e10f50de2971366d4b09f4f4f9249b0b7686f
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-08-03 11:56:16 +0530

    Time: 2 ms (73.81%) | Memory: 42.6 MB (91.17%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,20 +1,23 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        boolean res = false;
-        int i=0,j=0,c=0;
-        if(s.isEmpty()){
-            return true;
-        }
-        while(i<s.length() && j<t.length()){
-            if(s.charAt(i)==t.charAt(j)){
-                i++;
-                c++;
-            }
-            j++;
-        }
-        if(c==s.length()){
-            return true;
-        }
+    public boolean isSubsequence(String s, String t)
+    {
+     if(s.length()>t.length())
+     {
         return false;
+     }   
+     if(s.isEmpty())
+     {
+        return true;
+     }
+     int i=0,c=0,j=0;
+     while(i<s.length() && j<t.length())
+     {
+        if(s.charAt(i)==t.charAt(j))
+        {
+            i++;c++;
+        }
+        j++;
+     }
+     return c==s.length();
     }
 }
