a09eb5cb818775968ed04bc615411837a7f6b2bb
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-08-05 19:54:02 +0530

    Time: 3 ms (73.66%) | Memory: 69.9 MB (10.66%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,14 +1,19 @@
 class Solution {
-    public double findMaxAverage(int[] nums, int k) {
-        int sm=0;
-        for(int i=0;i<k;i++){
-            sm+=nums[i];
-        }
-        int max=sm;
-        for(int i=k;i<nums.length;i++){
-            sm+=nums[i]-nums[i-k];
-            max=Math.max(max,sm);
-        }
-        return (double)max/k;
+    public double findMaxAverage(int[] nums, int k) 
+    {
+    int ans=0;
+    int sm=0;
+    for(int i=0;i<k;i++)
+    {
+        sm+=nums[i];
+    }  
+    ans=sm; 
+    for(int i=k;i<nums.length;i++)
+    {
+        //ans=Math.max(ans,sm);
+        sm+=nums[i]-nums[i-k];
+        ans=Math.max(ans,sm);
+    }
+    return (double)ans/k;
     }
 }
