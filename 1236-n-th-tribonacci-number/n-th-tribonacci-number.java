4efdc7dda563a7ec9c0072800623171686ad5dad
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-08-26 20:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (53.63%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,16 +1,21 @@
 class Solution {
     public int tribonacci(int n) {
-        int[] a = new int[38];
-        a[0]=0;a[1]=1;a[2]=1;
-        if(n==0){
-            return 0;
-        }if(n==1 || n==2){
+        int[] a=new int[38];
+        a[0]=0;
+        a[1]=1;
+        a[2]=1;
+        if(n==0)
+        {
+            return a[0];
+        }
+        if(n<=2)
+        {
             return 1;
         }
-        for(int i=3;i<=n;i++){
+        for(int i=3;i<=n;i++)
+        {
             a[i]=a[i-1]+a[i-2]+a[i-3];
         }
         return a[n];
     }
-    
 }
