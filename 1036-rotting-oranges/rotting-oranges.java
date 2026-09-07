cfb4b7a31b6f3a5069b441ae4d4cc42dfbecb793
JUVERIA-28 <tarannumjuveria797@gmail.com>
2026-09-07 21:46:57 +0530

    Time: 2 ms (88.70%) | Memory: 44.4 MB (24.63%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,53 +1,55 @@
 class Solution {
-    public int orangesRotting(int[][] grid)
+    public int orangesRotting(int[][] grid) 
     {
-        int r=grid.length;
-        int c=grid[0].length;
-        Queue<int[]> q=new LinkedList<>();
-        int f=0;
-        for(int i=0;i<r;i++)
+     int r=grid.length;
+     int c=grid[0].length;
+     Queue<int[]>q=new LinkedList<>();
+     int cnt=0;
+     int[] x={0,0,1,-1};
+     int[] y={1,-1,0,0};
+     for(int i=0;i<r;i++)
+     {
+        for(int j=0;j<c;j++)
         {
-            for(int j=0;j<c;j++)
+            if(grid[i][j]==2)
             {
-                if(grid[i][j]==2){
-                    q.offer(new int[]{i,j});
-                }
-                else if(grid[i][j]==1){
-                    f++;
-                }
+                q.add(new int[]{i,j});
             }
         }
-        if(f==0)
+     } 
+     while(!q.isEmpty())
+     {
+        int n=q.size();
+        while(n-->0)
         {
-            return f;
-        }
-        int min=0;
-        int[][] dr = {
-            {1,0},{-1,0},{0,1},{0,-1}
-        };
-        while(!q.isEmpty() && f>0)
-        {
-            int s=q.size();
-            for(int k=0;k<s;k++)
+            int[] a=q.remove();
+            for(int i=0;i<4;i++)
             {
-                int[] cr=q.poll();
-                int a=cr[0];
-                int b=cr[1];
-                for(int[] d : dr)
-                {
-                    int x=a+d[0];
-                    int y=b+d[1];
-                    if(x>=0 && x<r && y>=0 && y<c && grid[x][y]==1)
-                    {
-                        grid[x][y]=2;
-                        f--;
-                        q.offer(new int[]{x,y});
-                    }
-                }
+               int x1=a[0]+x[i];
+               int y1=a[1]+y[i];
+               if(x1>=0 && x1<r && y1>=0 && y1<c && grid[x1][y1]==1)
+               {
+                    grid[x1][y1]=2;
+                    q.add(new int[]{x1,y1});
+               } 
             }
-            min++;
         }
-        return f==0?min:-1;
+        if(!q.isEmpty())
+        {
+            cnt++;
+        }
+     }  
+     for(int i=0;i<r;i++)
+     {
+        for(int j=0;j<c;j++)
+        {
+            if(grid[i][j]==1)
+            {
+                return -1;
+            }
+        }
+     }
+     return cnt;
+
     }
-    
 }
